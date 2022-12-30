1e572a5ea917a610b5b39d13504a882727821393
Aditya Anand <adityakumar0408@gmail.com>
2022-12-31 02:30:31 +0530

    new pattern file

---
+++ pattern3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class pattern3 {
+    public static void main(String args[]) {
+        int r=0;
+        System.out.println("Enter the Size of the Pattern:");
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        for(int i = 1; i <= c; i++) {
+            System.out.println(" ");
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            
+    }
+
+System.out.println();
+}
+}
+}
